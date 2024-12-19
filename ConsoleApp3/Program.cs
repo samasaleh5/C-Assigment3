d15863700f306fd622dd5b9262c934d18d72b729
samasaleh5 <ss2104@fayoum.edu.eg>
2024-12-19 19:30:44 +0200

    Question1 solved

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -20,20 +20,20 @@
         {
             #region Question1
             int number = 5;
-            Console.WriteLine($"Original value = {number}");
+            Console.WriteLine($"Original value ={number}");
             /*1-Passing Value type by value
              when passing a value type by value a copy of data is passed to funcction any
              change made to parameter inside function not affect original value
              */
             PassByValue(number);
-            Console.WriteLine($"After PassByValue = {number}");
+            Console.WriteLine($"After PassByValue ={number}");
 
             /*2-passing value type by ref
               when passing value type by ref the function directly works in orginal variable
               so any change made to paraameter inside functiom will affect orginal varaible
              */
             PassByReference(ref number);
-            Console.WriteLine($"After PassByReference = {number}");
+            Console.WriteLine($"After PassByReference ={number}");
             #endregion
 
         }
